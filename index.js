69878c74087e7bb439fd05fe632f7b6348cbcc84
Gabriel Terrazas <95561135+Kombini21@users.noreply.github.com>
2022-09-06 05:09:23 -0400

    Cria as páginas e cria as rotas com Express

--- index.js
+++ index.js
@@ -5,4 +5,29 @@ const express = require('express');
 
 const app = express();
 
-// 
+// Começa a receber requests
+
+app.listen(8080);
+
+// Retorna os requests para as páginas criadas
+
+app.get('/', (req, res) => {
+
+    res.render('./pages/index.html');
+});
+
+app.get('/sobre', (req, res) => {
+
+    res.render('./pages/about.html');
+});
+
+app.get('/contato', (req, res) => {
+
+    res.render('/pages/contact-me.html');
+});
+
+// Retorna qualquer outro tipo de request pra uma página 404
+
+app.use((req, res, next) => {
+    res.render('./pages/404.html');
+});
